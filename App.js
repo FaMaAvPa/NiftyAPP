1a759c37615b33c76cf749554e29aae3c285017d
Jano-Ch <janochiambretto@gmail.com>
2024-03-11 00:27:14 -0300

    empiezo con la camara, botones y permisos

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Login from './src/components/Login'
+import Camara from './src/components/Camara'
 import { View } from 'react-native-web';
 
 export default function App() {
   return(                         
-      <Login/>
+      <Camara/>
   );
 }
 
